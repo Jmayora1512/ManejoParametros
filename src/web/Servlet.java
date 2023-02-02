5edbce1824d10582b90d782767b73ea88a12a98d
Jmayora1512 <jorgemayora88@gmail.com>
2023-02-02 19:15:44 +0100

    Se agrega el proyecto

---
+++ src/web/Servlet.java
@@ -0,0 +1,59 @@
+package web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet
+ */
+@WebServlet("/Servlet")
+public class Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+        //Leer los parametros del formulario html
+        String usuario = request.getParameter("usuario");
+        String password = request.getParameter("password");
+        
+        System.out.println("usuario = " + usuario);
+        System.out.println("password = " + password);
+        
+        PrintWriter out = response.getWriter();
+        out.print("<html>");
+        out.print("<body>");
+        out.print("El parametro usuario es:" + usuario);
+        out.print("<br/>");
+        out.print("El parametro password es:" + password);
+        out.print("</body>");
+        out.print("</html>");
+        out.close();		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
